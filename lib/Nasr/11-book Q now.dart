2785444ea0b31d28d9db2b2f88fde721eed732c8
Ahmed Nasr <ahmednasr159@yahoo.com>
2021-04-07 01:10:16 +0200

    We 7/4/2020 nasr

--- lib/Nasr/11-book Q now.dart
+++ lib/Nasr/11-book Q now.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:queuey/Algaraiehy/12-select destination.dart';
 
-class BookQNow extends StatelessWidget {
+class BookQNow extends StatefulWidget {
+  @override
+  _BookQNowState createState() => _BookQNowState();
+}
+
+class _BookQNowState extends State<BookQNow> {
   List<String> _images = [
     'assets/images/Screens/11-book Q now/index1.png',
     'assets/images/Screens/11-book Q now/index2.png',
@@ -11,6 +17,7 @@ class BookQNow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
@@ -47,6 +54,133 @@ class BookQNow extends StatelessWidget {
               ),
             ],
           ),
+          Center(
+            child: Text(
+              'QueueY',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontStyle: FontStyle.italic,
+                fontSize: 48,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 140,
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+                itemCount: _images.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Image(image: AssetImage(_images[index]));
+                }),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  'Wasting time, taking effort, cause the virus ? ',
+                  style: TextStyle(
+                      //  fontSize: 17,
+                      ),
+                  overflow: TextOverflow.visible,
+                ),
+              ),
+              Icon(
+                Icons.tag_faces,
+                color: Theme.of(context).primaryColor,
+              )
+            ],
+          ),
+          Center(
+            child: Text(
+              'sure!',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  'So we did a ',
+                  style: TextStyle(
+                      //  fontSize: 17,
+                      ),
+                  overflow: TextOverflow.visible,
+                ),
+              ),
+              Text(
+                'QueueY',
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  ' to avoid all of that .. ',
+                  style: TextStyle(
+                      //  fontSize: 17,
+                      ),
+                  overflow: TextOverflow.visible,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SelectDestination()));
+              },
+              child: Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).primaryColor,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'Book your Queue Now!',
+                    style: TextStyle(
+                        fontSize: 25,
+                        //fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:queuey/1-SplashScreen.dart';
 import 'package:queuey/Nasr/11-book%20Q%20now.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xff40A3A3),
         fontFamily: 'OpenSans',
       ),
-      home: BookQNow(), //SplashScreen(0),
+      home: BookQNow(), //SplashScreen(),
     );
   }
 }
