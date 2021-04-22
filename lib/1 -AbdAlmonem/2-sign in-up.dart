cbc37280d0d346346cb7f6f99e4d03324a8a833f
Ahmed Nasr <ahmednasr159@yahoo.com>
2021-04-22 16:55:24 +0200

    th 22/4/2021 | nasr

--- lib/1 -AbdAlmonem/2-sign in-up.dart
+++ lib/1 -AbdAlmonem/2-sign in-up.dart
@@ -9,7 +9,7 @@ Route _createRoute() {
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       var begin = Offset(1.0, 0.0);
       var end = Offset.zero;
-      var curve = Curves.easeIn;
+      var curve = Curves.ease;
 
       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
